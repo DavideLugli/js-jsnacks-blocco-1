a00309635163a03221fc9eaa3d9e555e7ee93a2f
Davide Lugli <luglidavide.web@gmail.com>
2020-01-13 14:28:01 +0100

    primo blocco jsnacks

--- snack 7/js/script.js
+++ snack 7/js/script.js
@@ -1,1 +1,14 @@
-alert('ciaone');
+// Snack7
+// Stampa le potenze di 2 fino a 1000.
+for (var i = 1; i<=1000; i++) {
+  console.log(Math.pow(2,i));
+}
+
+// while
+// var i = 1;
+// while (i<=1000) {
+//   console.log(Math.pow(2,i));
+//   i++;
+// }
+//
+// }
--- snack 8/js/script.js
+++ snack 8/js/script.js
@@ -1,1 +1,16 @@
-alert('ciaone');
+// Snack8
+// Chiedi un numero di 4 cifre all’utente e calcola la somma
+// di tutte le cifre che compongono il numero.
+
+var numUtente = prompt('inserisci un n di 4 cifre');
+// non uso il parse int perchè ci complica la ''separazione'' del numero nelle 4 cifre. conviene usarlo come stringa, separare le cifre con un ciclo e poi convertirle in numero con un parse int
+var somma = 0;
+console.log(numUtente);
+
+for (var i = 0; i < numUtente.length; i++) {
+  // console.log(numUtente[i]);
+  console.log(parseInt(numUtente[i]));
+  somma += parseInt(numUtente[i]);
+}
+
+console.log(somma);
